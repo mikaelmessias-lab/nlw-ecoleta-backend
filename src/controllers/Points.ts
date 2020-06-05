fc0ac79548ec5dc32e7801af1af7cc9b5b50d9ac
Mikael Messias <mikaelmessias@outlook.com>
2020-06-05 15:22:28 -0300

    Add Multer
    Signed-off-by: Mikael Messias <mikaelmessias@outlook.com>

--- src/controllers/Points.ts
+++ src/controllers/Points.ts
@@ -17,7 +17,7 @@ class PointsController {
     const trx = await knex.transaction();
 
     const point = {
-      image: 'https://images.unsplash.com/photo-1488459716781-31db52582fe9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
+      image: request.file.filename,
       name,
       email,
       whatsapp,
@@ -31,12 +31,15 @@ class PointsController {
 
     const point_id = ids[0];
 
-    const pointItems = items.map((item_id: Number) => {
-      return {
-        item_id,
-        point_id,
-      }
-    });
+    const pointItems = items
+      .split(',')
+      .map((item: string) => Number(item.trim()))
+      .map((item_id: Number) => {
+        return {
+          item_id,
+          point_id,
+        }
+      });
 
     await trx('point_items').insert(pointItems);
 
@@ -63,7 +66,14 @@ class PointsController {
       .distinct()
       .select('points.*');
 
-    return response.json(points)
+    const serializedPoints = points.map(point => {
+      return {
+        ...point,
+        image_url: `http://192.168.31.155:3333/uploads/points/${point.image}`,
+      };
+    })
+
+    return response.json(serializedPoints)
   };
 
   async show(request: Request, response: Response) {
@@ -77,6 +87,11 @@ class PointsController {
       });
     }
 
+    const serializedPoint = {
+      ...point,
+      image_url: `http://192.168.31.155:3333/uploads/points/${point.image}`,
+    };
+
     // SELECT * FROM items 
     //  FROM point_items ON items.id = point_items.item_id
     // WHERE point_items.point_id = {id}
@@ -85,7 +100,7 @@ class PointsController {
       .where('point_items.point_id', id)
       .select('items.title');
 
-    return response.json({ point, items });
+    return response.json({ point: serializedPoint, items });
   };
 
 
--- src/routes.ts
+++ src/routes.ts
@@ -1,16 +1,21 @@
 import express from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import PointsController from './controllers/Points';
 import ItemsController from './controllers/Items';
 
 const routes = express.Router();
+const upload = multer(multerConfig);
+
 const pointsController = new PointsController();
 const itemsController = new ItemsController();
 
-routes.get('/items', itemsController.index);
+routes.post('/points', upload.single('image'), pointsController.create);
 
-routes.post('/points', pointsController.create);
+routes.get('/items', itemsController.index);
 routes.get('/points', pointsController.index);
+
 routes.get('/points/:id', pointsController.show);
 
 export default routes;
